6321bb845ee711ab1b01fa6013c2ece7a9c31790
namrata2612 <51783686+namrata2612@users.noreply.github.com>
2020-07-02 23:26:51 -0700

    portal sensegiz

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -26,6 +26,7 @@ export class SettingsComponent implements OnInit {
   min:any=[0,1,2,3,4,5,6,7,8,9,10]
   sec:any=[0,5,10,15,20,25,30,35,40,45,50,55]
   minStatus:boolean=false
+  duration:any
   constructor(public dialog: MatDialog,private fb:FormBuilder,private api:ApiService,private login:LoginCheckService,private general:GeneralMaterialsService) { }
 
   ngOnInit(): void {
@@ -81,6 +82,11 @@ export class SettingsComponent implements OnInit {
       //console.log("setting data page ======",res);
       if(res.status){
         this.setting = res.success[0]
+        
+        this.duration=res.success[0].durationThreshold
+        var minutes = Math.round(this.duration/60);
+        var seconds = this.duration%60;
+
         this.distanceForm.patchValue({
           distance: res.success[0].distance.toString(),
           rssi: res.success[0].rssi
@@ -97,7 +103,11 @@ export class SettingsComponent implements OnInit {
         this.bufferForm.patchValue({
           buffer: res.success[0].buffer,
         })
-
+        this.timeForm.patchValue({
+          minutes:minutes,
+          seconds:seconds
+        })
+        
         if( res.success[0].inactivityStatus == 1){
           this.inactivityStatusValue = {
             value:true,
