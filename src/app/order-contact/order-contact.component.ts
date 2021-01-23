6ee55c26f95866cb8091557b838818b659026438
RAJESHKUMARVANKUDOTHU <68320301+RAJESHKUMARVANKUDOTHU@users.noreply.github.com>
2021-01-23 08:49:29 +0530

    changed

--- src/app/order-contact/order-contact.component.ts
+++ src/app/order-contact/order-contact.component.ts
@@ -173,6 +173,22 @@ export class OrderContactComponent implements OnInit {
     const dialogRef = this.dialog.open(OrderContactComponent, dialogConfig);
 
     dialogRef.afterClosed().subscribe(result => {
+      this.order = parseInt(this.order) - 1
+      const dialogConfig = new MatDialogConfig();
+      dialogConfig.disableClose = true;
+      dialogConfig.autoFocus = true;
+      dialogConfig.height = '90vh';
+      dialogConfig.width = '75vw';
+      dialogConfig.data = {
+        data:a,
+        userId:a.userId,
+        subUserId:a.subUserId,
+        order:this.order,
+        fromDate : this.from,
+        toDate : this.to
+      }
+
+
     });
   }
 
