4af7cb183c95047e7c324482acac4fe8c1107a35
RAJESHKUMARVANKUDOTHU <68320301+RAJESHKUMARVANKUDOTHU@users.noreply.github.com>
2021-02-16 15:13:07 +0530

    changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   apiHost:'http://13.235.59.39:3000',
   // apiHost:'http://44.234.23.217:3000',
-  socketHost:'http://13.235.59.39:3000',
+  socketHost:'http://13.235.59.39:3010',
 
   production: true
 };
